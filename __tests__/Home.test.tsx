112f0cb755df459c96871780d67428a5dddd4050
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-13 13:34:00 +0900

    Homeコンポーネントのテストコードを作成

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import Home from '../pages'
+
+it('Should render hello text', () => {
+  render(<Home />)
+  expect(screen.findByText('Hello Nextjs')).toBeInTheDocument()
+})
